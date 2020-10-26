196e0f19b8524d0f0b2855c9f776a84b3027f316
yatharth <yatharthverma070@gmail.com>
2020-10-26 14:01:22 +0530

    Add introduction and demo link

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### NEXT.js Blog
 
 Learning nextjs from docs in official website of [Nextjs](https://nextjs.org/)
+
+[Demo](https://nextjs-blog-ebon-phi.vercel.app/)
--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,8 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Hy, I am passionate fullstack web developer. I like to learn new things every day.</p>
+        
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
